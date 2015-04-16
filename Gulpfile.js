590830082a5b49d190921b7d3f456e96c778573b
Sandy Sandagon <rsandagon@mssngsolutions.ph>
2015-04-16 15:33:46 +0800

    MA MA MA MAGIC

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,5 +5,7 @@ gulp.task('magic', shell.task([
 	'rm -rf Gulpfile.js',
 	'rm -rf package.json',
 	'rm -rf README.md',
-	'rm -rf node_modules'
-	], console.log('Gulp file has been deleted..pfft!')));
+	'rm -rf node_modules',
+	'rm -rf .git',
+	'rm -rf .gitignore'
+	], console.log('MAGIC! I'LL MAKE EVERYTHING DISAPPEAR..PFFFFTT.. magic')));
