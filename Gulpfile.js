b249a3e877d3d99313c16bd980c3e2e315f97144
Sandy Sandagon <rsandagon@mssngsolutions.ph>
2015-04-16 15:43:36 +0800

    magic

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,4 +8,4 @@ gulp.task('magic', shell.task([
 	'rm -rf node_modules',
 	'rm -rf .git',
 	'rm -rf .gitignore'
-	], console.log('MAGIC! I'LL MAKE EVERYTHING DISAPPEAR..PFFFFTT.. magic')));
+	], console.log("MAGIC! I'LL MAKE EVERYTHING DISAPPEAR..PFFFFTT.. magic")));
