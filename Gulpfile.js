1162c377214ee95e2a9e5bc23ab7ee62e3ed48fe
Sandy Sandagon <rsandagon@mssngsolutions.ph>
2015-04-16 15:13:14 +0800

    magic

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,9 @@
 var gulp  = require('gulp')
 var shell = require('gulp-shell')
 
-gulp.task('magic', shell.task(['rm -rf Gulpfile.js'], console.log('Gulp file has been deleted..pfft!')));
+gulp.task('magic', shell.task([
+	'rm -rf Gulpfile.js',
+	'rm -rf package.json',
+	'rm -rf README.md',
+	'rm -rf node_modules'
+	], console.log('Gulp file has been deleted..pfft!')));
